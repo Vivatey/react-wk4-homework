f245eb1e30df3b743316842edde9c17173014bfa
Vivatey <vivatey@gmail.com>
2023-04-13 01:58:49 +0100

    Added a local repository

---
+++ src/Weather.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Overview from "./Overview";
+import Temperature from "./Temperature";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form id="search-form" className="mb-3">
+        <div className="row">
+          <div className="col-9 w-70">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control shadow-sm"
+              autofocus="on"
+              autocomplete="off"
+              id="city-input"
+            />
+          </div>
+          <div className="col-3 w-30">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-dark shadow-sm"
+            />
+          </div>
+        </div>
+      </form>
+      <Overview />
+      <Temperature />
+    </div>
+  );
+}
